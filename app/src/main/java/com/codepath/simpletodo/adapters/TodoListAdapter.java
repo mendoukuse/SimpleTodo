373598f2d2d2667c6485cf15be15780c00c82bcd
Christine Nguyen <christine.nguyen@airbnb.com>
2017-03-05 15:11:05 -0800

    display due date and priority in list view

--- app/src/main/java/com/codepath/simpletodo/adapters/TodoListAdapter.java
+++ app/src/main/java/com/codepath/simpletodo/adapters/TodoListAdapter.java
@@ -1,16 +1,19 @@
 package com.codepath.simpletodo.adapters;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.CheckBox;
 import android.widget.TextView;
 
 import com.codepath.simpletodo.R;
+import com.codepath.simpletodo.models.Priority;
 import com.codepath.simpletodo.models.TodoItem;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 /**
@@ -32,8 +35,31 @@ public class TodoListAdapter extends ArrayAdapter<TodoItem> {
         }
         // Lookup view for data population
         TextView title = (TextView) convertView.findViewById(R.id.itemTitle);
-        // Populate the data into the template view using the data object
+        TextView dueDate = (TextView) convertView.findViewById(R.id.itemDueDate);
+        TextView priority = (TextView) convertView.findViewById(R.id.itemPriority);
+
         title.setText(item.getTitle());
+
+        if (item.getPriority() != null) {
+            priority.setText(item.getPriority().name());
+            switch(item.getPriority()) {
+                case HIGH:
+                    priority.setTextColor(Color.RED);
+                    break;
+                case MEDIUM:
+                    priority.setTextColor(Color.CYAN);
+                    break;
+                case LOW:
+                default:
+                    priority.setTextColor(Color.GREEN);
+                    break;
+            }
+        }
+
+        if (item.getDueDate() != null) {
+            dueDate.setText(new SimpleDateFormat("MM/dd/yyyy").format(item.getDueDate()));
+            dueDate.setTextSize(10);
+        }
 
         // Return the completed view to render on screen
         return convertView;
